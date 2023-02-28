b444f12a23f04ec5ebb3703d1e8668aca036e3c1
Cory Pollard <astandard87@gmail.com>
2023-02-28 13:32:40 -0600

    Added notes on keyof constraints

--- src/app.ts
+++ src/app.ts
@@ -40,3 +40,34 @@ console.log(mergedObj);
 //example: merge<T extends object, U extends object>(){}
 
 // Please note: These constraints can be anything. string, number, custom types, union types, etc.
+
+// Another function example
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements.`;
+  }
+  return [element, descriptionText];
+}
+
+// console.log(countAndDescribe('Hi there!'));
+// console.log(countAndDescribe(['Sports', 'Cooking']));
+// console.log(countAndDescribe([]));
+
+// keyof Constraint
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return `Value: ${obj[key]}`;
+}
+
+console.log(extractAndConvert({ name: 'douglas' }, 'name'));
